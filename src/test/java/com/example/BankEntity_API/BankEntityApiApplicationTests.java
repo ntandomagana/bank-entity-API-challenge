0101285b38d20833760cbb661cf23b90c581262f
Ntando Magana <ntandomagana@gmail.com>
2025-09-19 23:12:42 +0200

    initial commit - bankEntityAPI

---
+++ src/test/java/com/example/BankEntity_API/BankEntityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BankEntity_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankEntityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
